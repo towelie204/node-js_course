534e6f6f32ba25cb98d1dc465ea0dfcd0f324681
towelie <towelie204@gmail.com>
2020-11-30 19:58:49 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const colors = require('colors/safe');
+
+const showLog = (message) => console.log(message);
+
+showLog(colors.green('green text'));
+
+showLog(colors.underline.red('underLine red')); 
+
+showLog(colors.inverse('inveresed color text')); 
+
+showLog(colors.bgRed('red background')); 
+
+showLog(colors.brightYellow.bold('bright yellow bold text')); 
+
+showLog(colors.random('rendom colored text')); 

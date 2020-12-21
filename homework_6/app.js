36601d90b820bcb5f7a2e02e9f1465f0c535f39a
towelie <towelie204@gmail.com>
2020-12-21 04:34:36 +0300

    homework 6 and 7

--- homework_6/app.js
+++ homework_6/app.js
@@ -3,8 +3,13 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
+const passport = require('passport');
+const session = require('express-session');
+const FileStore = require('session-file-store')(session);
 
 const indexRouter = require('./routes/index');
+const authRouter = require('./routes/auth');
+require('./config/passport')(passport);
 
 const db = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
 mongoose.connect(db, { 
@@ -17,6 +22,22 @@ mongoose.connect(db, {
 
 const app = express();
 
+//express sessions
+app.use(session({
+    secret: 'all-cats-are-beatiful',
+    store: new FileStore(),
+    cookie: {
+        path: '/',
+        httpOnly: true,
+        maxAge: 3600000,
+    },
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(methodOverride('_method')); //библиотека для использования методов PUT, DELETE
 
 app.use(express.json());
@@ -27,7 +48,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
-app.use('/', indexRouter);
+app.use('/notes', indexRouter);
+app.use('/', authRouter);
 
 app.listen(3000);
 console.log('server listening on port 3000...');
